89d9bbd4b888a8fa861eadf12abbe7ac687faf20
y0m0 <lixi.daniele@gmail.com>
2017-07-15 18:04:58 +0100

    User can now add patients - adds patients form

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715132716) do
+ActiveRecord::Schema.define(version: 20170715153357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "doctors", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "medical_records", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,6 +23,11 @@ ActiveRecord::Schema.define(version: 20170715132716) do
   create_table "patients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "gender"
   end
 
   create_table "users", force: :cascade do |t|
